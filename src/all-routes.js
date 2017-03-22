68195f4f9f9457b15a880fe020076c660837fc32
zkboys <876213774@qq.com>
2017-03-22 21:56:56 +0800

    init a new asstes2

--- src/all-routes.js
+++
@@ -1,9 +0,0 @@
-import ComponentsHelloRoutes from './components/hello/routes';
-import ComponentsHomeRoutes from './components/home/routes';
-import ComponentsLoginRoutes from './components/login/routes';
-
-export default [].concat(
-    ComponentsHelloRoutes,
-    ComponentsHomeRoutes,
-    ComponentsLoginRoutes
-);
